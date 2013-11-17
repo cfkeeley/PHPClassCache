398551f18b058ad06135015fb77f8388d4a3b6ee
Chris Keeley <chris@relationshapes.com>
2013-11-17 09:42:26 +0000

    Initial code base

---
+++ tests/ClassLoaderTest.php
@@ -0,0 +1,14 @@
+<?php
+require_once('ClassLoader.class.php');
+/**
+ * Relationshapes 
+ * Copyright Jun 2, 2013 Chris Keeley <info@relationshapes.com> All rights reserved. 
+ */
+class ClassLoaderTest extends PHPUnit_Framework_TestCase
+{
+    public function testClassLoader()
+    {
+     $loader = new ClassLoader();   
+    }
+}
+?>
